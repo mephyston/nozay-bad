e02bf80bcb453173a5a7d0820609689fc8990735
David PAGNACCO <david.pagnacco@gmail.com>
2026-07-07 12:09:58 +0200

    types: add env.d.ts definitions for admin-console and boutique

---
+++ apps/admin-console/src/env.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="astro/client" />
+declare namespace App {
+  interface Locals {
+    user?: {
+      email: string;
+    };
+    runtime: import('@astrojs/cloudflare').Runtime<Env>;
+  }
+}
+interface Env {
+  API_SERVICE: import('@cloudflare/workers-types').Fetcher;
+  CF_TEAM_DOMAIN?: string;
+  CF_AUDIENCE?: string;
+  DB: import('@cloudflare/workers-types').D1Database;
+}
