0849d834d2be20a9c797cf9a797f62a5007a6f0f
David PAGNACCO <david.pagnacco@gmail.com>
2026-07-26 14:27:06 +0200

    fix(api): allow tresorerie type in update account class validator

--- libs/domains/accounting/config/update-account-class/validator.ts
+++ libs/domains/accounting/config/update-account-class/validator.ts
@@ -2,5 +2,5 @@ import { Type } from '@sinclair/typebox';
 
 export const updateAccountClassSchema = Type.Object({
   label: Type.Optional(Type.String()),
-  type: Type.Optional(Type.Union([Type.Literal('recette'), Type.Literal('depense')]))
+  type: Type.Optional(Type.Union([Type.Literal('recette'), Type.Literal('depense'), Type.Literal('tresorerie')]))
 });
